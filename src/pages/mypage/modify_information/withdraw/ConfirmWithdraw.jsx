d6a69639d2f71efb93a8ec1d6211cfc077532211
kimeunji <eunji93121@gmail.com>
2024-07-31 12:59:44 +0900

    회원탈퇴 기능 수정

--- src/pages/mypage/modify_information/withdraw/ConfirmWithdraw.jsx
+++ src/pages/mypage/modify_information/withdraw/ConfirmWithdraw.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import MtButtons from '../../../../components/common/buttons/MtButtons';
 import styles from "./Withdraw.module.scss";
 import DefaultInput from '../../../../components/common/inputs/DefaultInput';
 
 const ConfirmWithdraw = ({ email, password }) => {
+  const navigate = useNavigate();
+
+  const handleWithdraw = () => {
+    // 실제 탈퇴 처리 로직 추가 필요
+    // 탈퇴 처리 완료 후 intro 페이지로 이동
+    navigate('/intro');
+  };
+
   return (
     <div className={styles.content}>
       <h1 className={`title ${styles.text}`}>정말 탈퇴하시겠습니까</h1>
@@ -22,7 +31,7 @@ const ConfirmWithdraw = ({ email, password }) => {
       <div className={styles.button}>
         <MtButtons 
           eventType={'click'} 
-          // 실제 탈퇴 처리 로직 추가 필요
+          eventHandler={handleWithdraw} 
           buttonType={'apply'} 
           buttonText={'탈퇴하기'}
         />

c6e6ef253b70bd3f155fa6cb5d11e5260f33f74d
yocong <rnfaos77@naver.com>
2024-08-01 16:10:49 +0900

    🐛 css수정 및 에러해결 및 프로필 모달 추가

--- src/pages/login/components/FirstLoginProfile.jsx
+++ src/pages/login/components/FirstLoginProfile.jsx
@@ -1,12 +1,16 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./FirstLoginProfile.module.scss";
 import MtButtons from "../../../components/common/buttons/MtButtons";
 import defaultImg from "../../../assets/images/mypage/coong.jpg";
+import ProfileMenuModal from "./ProfileModal/ProfileMenuModal";
 
 const FirstLoginProfile = ({ nextHandler }) => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [profileImg, setProfileImg] = useState(defaultImg);
+  const [modalActive, setModalActive] = useState(false);
+  const [modalPosition, setModalPosition] = useState({ x: 0, y: 0 });
   const fileInputRef = useRef(null);
+
 
   // 파일 변경 핸들러
   const fileChangeHandler = (e) => {
@@ -26,15 +30,35 @@ const FirstLoginProfile = ({ nextHandler }) => {
     setProfileImg(defaultImg);
   };
 
+
   const submitHandler = () => {
-    if (!selectedFile) {
-      alert("프로필 이미지를 선택하세요.");
-      return;
-    }
 
     ////////////// 서버 업로드 하는 부분 ///////////////
 
     nextHandler();
+  };
+
+  // 모달에서 기본 이미지로 설정
+  const setDefaultImageHandler = () => {
+    setProfileImg(defaultImg);
+    setSelectedFile(null);
+    setModalActive(false);
+  };
+
+  // 모달에서 프로필 변경 선택
+  const changeProfileHandler = () => {
+    fileInputRef.current.click();
+    setModalActive(false);
+  };
+
+  // 프로필 이미지 클릭 핸들러
+  const profileClickHandler = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    setModalPosition({
+      x: rect.left + rect.width / 2 + 40,
+      y: rect.top + window.scrollY + 80// 모달을 이미지 위쪽으로 약간 띄우기 위해 -10을 추가합니다.
+    });
+    setModalActive(!modalActive);
   };
 
   return (
@@ -42,7 +66,8 @@ const FirstLoginProfile = ({ nextHandler }) => {
       <h1 className={`title ${styles.text}`}>프로필 설정</h1>
       <div
         className={styles.profile}
-        onClick={() => fileInputRef.current.click()}>
+        onClick={profileClickHandler}
+      >
         <img src={profileImg} alt="프로필 이미지" />
       </div>
       <input
@@ -52,11 +77,12 @@ const FirstLoginProfile = ({ nextHandler }) => {
         onChange={fileChangeHandler}
         className={`${styles.fileInput}`}
       />
-      {selectedFile && (
-        <button onClick={removeFileHandler} className={styles.removeButton}>
-          이미지 제거
-        </button>
-      )}
+      {selectedFile && <ProfileMenuModal
+        active={modalActive}
+        position={modalPosition}
+        onDefaultImage={setDefaultImageHandler}
+        onChangeProfile={changeProfileHandler}
+      />}
       <MtButtons
         buttonType={"apply"}
         buttonText={"SUBMIT"}
@@ -66,6 +92,13 @@ const FirstLoginProfile = ({ nextHandler }) => {
       <div className={styles.skip} onClick={nextHandler}>
         건너뛰기
       </div>
+
+      <ProfileMenuModal
+        active={modalActive}
+        position={modalPosition}
+        onDefaultImage={setDefaultImageHandler}
+        onChangeProfile={changeProfileHandler}
+      />
     </div>
   );
 };

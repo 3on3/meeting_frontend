2c6bfe5eae695dc95d14f772289b5ec0163e8462
hun2zz <tkdgnsdldkdlel@naver.com>
2024-08-06 09:11:34 +0900

    ✅ 경로 수정 및 group 추가

--- src/layout/components/FloatingNavigation.jsx
+++ src/layout/components/FloatingNavigation.jsx
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
-const FloatingNavigation = ({styles}) => {
+const FloatingNavigation = ({ styles }) => {
   const [active, setActive] = useState(false);
+  const navigate = useNavigate();
   const floatingButtonOnClick = () => {
     console.log("click");
-    setActive(!active)
-  }
+    setActive(!active);
+  };
   return (
     <div className={styles.floating}>
-    <button onClick={floatingButtonOnClick} className={active? styles.isActive : ""}><i></i></button>
-    <nav className={active? styles.isActive : ""}>
-      <NavLink className={styles.invite_code_btn}>참여 코드</NavLink>
-      <NavLink className={styles.new_group_btn}>새 그룹</NavLink>
-    </nav>  
-  </div>
+      <button
+        onClick={floatingButtonOnClick}
+        className={active ? styles.isActive : ""}
+      >
+        <i></i>
+      </button>
+      <nav className={active ? styles.isActive : ""}>
+        <NavLink className={styles.invite_code_btn}>참여 코드</NavLink>
+        <NavLink className={styles.new_group_btn} to="/mypage/group/create">
+          새 그룹
+        </NavLink>
+      </nav>
+    </div>
   );
 };
 

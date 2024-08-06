8be99a51b9c88926392e2da1fd5947fbec18a536
JinWooP98 <qwdk0406@naver.com>
2024-08-06 15:49:48 +0900

    :white_check_mark: 채팅 메시지에 대한 서버에서 받는 정보 Dto로 처리, 리팩토링

--- src/assets/js/test-chat/TestChat.js
+++ src/assets/js/test-chat/TestChat.js
@@ -4,7 +4,8 @@ import ChatBody from "../../../pages/chat/components/ChatBody";
 import ChatInput from "../../../pages/chat/components/ChatInput";
 import styles from "../../../pages/chat/Chat.module.scss";
 import ChatMembersModal from "../../../pages/chat/components/member_modal/ChatMembersModal";
-import {userDataLoader} from "../../../config/auth";
+import {chatWebSocket} from "./TestChatWebSocket";
+import {fetchMessage, saveMessage} from "./TestChatFetch";
 
 const TestChat = () => {
   // input value
@@ -13,57 +14,15 @@ const TestChat = () => {
   // 채팅 배열
   const [messageList, setMessageList] = useState([]);
 
+  // 웹소켓
   const [socket, setSocket] = useState(null);
 
-  // 처음 채팅방 입장시 그동안의 메세지 받아오기
-  useEffect(   () => {
-
-    fetchData();
-
-  }, []);
-
-  // 메세지 받아오는 fetch
-  async function fetchData()  {
-    const response = await fetch("http://localhost:8253/getMessage?chatRoomId=www");
-
-    const data = await response.json();
-
-    setMessageList(data);
-
-    console.log(data);
-  }
-
-  // 웹소켓 설정
   useEffect(() => {
-    // WebSocket 설정
-    const newSocket = new WebSocket("ws://localhost:8253/testChat");
+    // 웹소켓 설정
+    chatWebSocket(setSocket, setMessageList);
 
-    setSocket(newSocket);
-
-    newSocket.onopen = () => {
-      console.log("WebSocket connected");
-    };
-
-    newSocket.onmessage = (event) => {
-      const newMessage = JSON.parse(event.data);
-      console.log(newMessage);
-      setMessageList(prevState => [...prevState, newMessage]);
-    };
-
-    newSocket.onclose = () => {
-      console.log("WebSocket disconnected");
-    };
-
-    newSocket.onerror = (error) => {
-      console.log("WebSocket error:", error);
-    };
-
-
-
-    // 컴포넌트 언마운트 시 소켓 연결 해제
-    return () => {
-      newSocket.close();
-    };
+    // 채팅방을 열면 이 채팅방의 메시지 받아오기
+    fetchMessage(setMessageList);
 
   }, []);
 
@@ -76,49 +35,19 @@ const TestChat = () => {
   const onClickSendBtn = async () => {
 
     if (value !== "") {
-      const message = {
+      const payload = {
         roomId: "www",
         message: value,
       }
 
-      const data = await sendMessage(message);
+      const data = await saveMessage(payload);
 
       socket.send(JSON.stringify(data));
-      setMessageList(prevState => [...prevState, data]);
+      // setMessageList(prevState => [...prevState, data]);
       }
 
       setValue("");
   };
-
-  const sendMessage = async (payload) => {
-
-    const loginUser = userDataLoader();
-
-    const response = await fetch(`http://localhost:8253/sendMessage`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-            "Bearer " +
-            loginUser.token
-
-      },
-      body: JSON.stringify(payload),
-    });
-
-    const json = await response.json();
-
-    return json;
-  }
-
-
-  useEffect(() => {}, [messageList]);
-
-  // 참여자 보기 버튼
-  // const [modalActive, setModalActive] = useState(false);
-  // const onClickViewMemberBtn = ()=>{
-  //   setModalActive(true)
-  // }
 
   return (
     <div className={styles.container}>
--- src/assets/js/test-chat/TestSocket.js
+++
@@ -1,6 +0,0 @@
-import {io} from "socket.io-client";
-
-
-const testSocket = io("http://localhost:8253/testChat");
-
-export default testSocket;

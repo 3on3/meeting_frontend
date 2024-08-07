c7590154f1ede08b556c4ece6664020aabb5dcf4
yaejin12 <pos03167@naver.com>
2024-08-07 14:08:03 +0900

    ✅ 그룹페이지 연결

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -117,7 +117,7 @@ export const router = createBrowserRouter([
         element: <ErrorPage />,
       },
       {
-        path: "group",
+        path: "group/:groupId",
         element: <Group />,
       },
     ],

32193e2a360a99e48b856eeef8b0ca4c1009ae3f
hun2zz <tkdgnsdldkdlel@naver.com>
2024-08-08 14:32:29 +0900

    :bug: 리액트 병합 수정

--- src/pages/group/components/GroupViewHead.jsx
+++ src/pages/group/components/GroupViewHead.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import RegionFilterBox from "../../../components/common/regionFilterBoxs/RegionFilterBox";
 import Information from "./Information";
 
-const GroupViewHead = ({ styles, name, location, information,auth }) => {
+const GroupViewHead = ({ styles, age, totalMember, gender, auth, place }) => {
   return (
     <>
       <div className={styles.content}>
-        {auth === "HOST" && <button className={styles.groupDelBtn}>그룹 삭제</button>}
-        
+        {auth === "HOST" && (
+          <button className={styles.groupDelBtn}>그룹 삭제</button>
+        )}
+
         <h1 className={`title ${styles.text}`}>임시</h1>
         <div className={styles.labelsWrap}>
           <ul className={styles.regionFilter}>

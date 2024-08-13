c1b4f6e0f34617fb47e6c45f43f9b5b1493429ed
JinWooP98 <qwdk0406@naver.com>
2024-08-13 11:24:58 +0900

    :white_check_mark: 내 채팅 리스트를 확인할 수 있는 페이지 생성 및 채팅방과 연결

--- src/layout/components/MainNavigation.jsx
+++ src/layout/components/MainNavigation.jsx
@@ -14,7 +14,7 @@ const MainNavigation = ({ styles }) => {
       <NavLink to="/mypage/mygroup" className={activeClassFn}>
         <i className={styles.group}></i>내 그룹
       </NavLink>
-      <NavLink to="/Chat" className={activeClassFn}>
+      <NavLink to="/mypage/myChat" className={activeClassFn}>
         <i className={styles.chat}></i>내 채팅
       </NavLink>
       <NavLink to="/mypage" className={activeClassFn}>
--- src/pages/mypage/mypage_chats/MyChats.js
+++ src/pages/mypage/mypage_chats/MyChats.js
@@ -1,12 +1,25 @@
-import React from "react";
-import GroupBox from "../../../components/common/groupBoxs/GroupBox";
+import React, {useEffect, useState} from "react";
 import styles from "./MyChats.module.scss";
+import MyChatList from "./components/MyChatList";
+import {myChatListFetch} from "./MyChatFetch";
 const MyChats = () => {
+
+    const [chatList, setChatList] = useState(null);
+
+    useEffect( () => {
+        const data = myChatListFetch(setChatList);
+
+        console.log(data);
+    }, []);
+
+    useEffect(() => {
+        console.log(chatList)
+    }, [chatList]);
+
   return (
     <ul className={styles.myChatsWrapper}>
       <p className={styles.myGroupsTitle}>내가 속한 채팅</p>
-      <GroupBox state={"line"} />
-      <GroupBox state={"line"} />
+        {chatList && chatList.map(chat => <MyChatList key={chat.chatRoomId} chat={chat}/>)}
     </ul>
   );
 };

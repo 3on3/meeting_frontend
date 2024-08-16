535a7ed7f742d46b9223a10f532abb1e7c0b6105
yaejin12 <pos03167@naver.com>
2024-08-17 01:26:37 +0900

    ✅ 무한스크롤

--- src/pages/main/components/MeetingList.jsx
+++ src/pages/main/components/MeetingList.jsx
@@ -10,10 +10,13 @@ import { throttle } from "lodash";
 
 // matchingState={"complete"} 이면 매칭 완료
 function MeetingList() {
+  // 페이지 번호
+  const [pageNo, setPageNo] = useState(1);
   //URL 파라미터를 가져오고 관리할 수 있는 훅
   const [searchParams, setSearchParams] = useSearchParams();
-  const pageNo = Number(searchParams.get("pageNo")) ?? 1;
   const gender = searchParams.get("gender");
+  console.log("gender : ", gender);
+
   const region = searchParams.get("region");
   const personnel = searchParams.get("personnel");
 
@@ -60,37 +63,48 @@ function MeetingList() {
 
       // 데이터 받기
       const list = await response.json();
+
       const { content, totalElements } = list;
       const updatedListData = [...listData, ...content];
       console.log("updatedListData : ", updatedListData);
-      setListData(updatedListData);
 
-      //더 불러올 데이터가 있는지
-      if (totalElements === content.length) {
-        setIsFinish(true);
+      // setTimeout(() => {
+
+      if (pageNo === 1) {
+        setListData(content);
+      } else {
+        setListData(updatedListData);
       }
 
+      // 다음 페이지
+      setPageNo((prevPage) => prevPage + 1);
+
       setLoading(false);
+
+      //더 불러올 데이터가 있는지
+      if (totalElements === updatedListData.length) {
+        setIsFinish(true);
+      }
+      // }, 500);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
-  // 컴포넌트가 마운트될 때 데이터 페칭
-  useEffect(() => {
-    MainMeetingListFetch();
-  }, [gender, region, personnel]); // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때 한 번만 실행되도록 설정
-
   // 1초 간격으로 호출 제한
   const throttledFetch = throttle(MainMeetingListFetch, 1000);
 
+  // 컴포넌트가 마운트될 때 데이터 페칭
+  useEffect(() => {
+    setListData([]);
+    setPageNo(1);
+    setIsFinish(false);
+    MainMeetingListFetch();
+  }, [gender, region, personnel]); // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때 한 번만 실행되도록 설정
+
   useEffect(() => {
     if (inView && !loading && !isFinish) {
-      throttledFetch();
-      setSearchParams((prev) => ({
-        ...prev,
-        pageNo: pageNo + 1,
-      }));
+      MainMeetingListFetch();
     }
   }, [inView]);
 

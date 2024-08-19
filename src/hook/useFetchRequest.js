9274aa706b4a227b7e84ae4c768e109024592679
munjieun <mun.mimi925@gmail.com>
2024-08-19 17:52:58 +0900

    :bug: 포스트 토큰 수정

--- src/hook/useFetchRequest.js
+++ src/hook/useFetchRequest.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { CHATROOM_URL, MYPAGE_URL, MYPAGEMATCHING_URL } from "../config/host-config";
+import {
+  CHATROOM_URL,
+  MYPAGE_URL,
+  MYPAGEMATCHING_URL,
+} from "../config/host-config";
 import { useNavigate } from "react-router-dom";
 import { getUserToken } from "../config/auth";
 
@@ -8,15 +12,15 @@ export const useFetchRequest = () => {
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-
   // 매칭 요청 페치
-  const requestFetch = async (payload , setIsChanged) => {
+  const requestFetch = async (payload, setIsChanged) => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(`${MYPAGEMATCHING_URL}/createRequest`, {
         method: "POST",
         headers: {
+          Authorization: "Bearer " + getUserToken(),
           "Content-Type": "application/json",
         },
         body: JSON.stringify(payload),
@@ -27,7 +31,6 @@ export const useFetchRequest = () => {
       } else {
         const errorText = await response.text();
         setError(errorText);
-
       }
     } catch (err) {
       console.error("Error:", err);
@@ -35,19 +38,17 @@ export const useFetchRequest = () => {
     } finally {
       setIsLoading(false);
     }
-  }
-
-
+  };
 
   const alarmFetch = async (responseGroupId) => {
     console.log(responseGroupId);
 
     const payload = {
       responseGroupId: responseGroupId,
-    }
+    };
 
     const response = await fetch(`${MYPAGEMATCHING_URL}/alarm`, {
-      method:'POST',
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
@@ -59,9 +60,7 @@ export const useFetchRequest = () => {
     const data = await response.json();
 
     return data;
-
-
-  }
+  };
   // 매칭 프로세스 페치 - 수락(requsetUrl = "response-accept")/거절(requsetUrl = "response-deny")
   const processFetch = async (requestUrl, payload, setIsChanged) => {
     setIsLoading(true);
@@ -71,6 +70,7 @@ export const useFetchRequest = () => {
       const response = await fetch(`${MYPAGEMATCHING_URL}/${requestUrl}`, {
         method: "POST",
         headers: {
+          Authorization: "Bearer " + getUserToken(),
           "Content-Type": "application/json",
         },
         body: JSON.stringify(payload),
@@ -101,6 +101,7 @@ export const useFetchRequest = () => {
       const response = await fetch(`${CHATROOM_URL}/create`, {
         method: "POST",
         headers: {
+          Authorization: "Bearer " + getUserToken(),
           "Content-Type": "application/json",
         },
         body: JSON.stringify(payload),
@@ -124,7 +125,6 @@ export const useFetchRequest = () => {
     }
   };
 
-
   // 마이 그룹 리스트 페치
   // const MyGroupsListFetch = async () => {
   //   try {
@@ -141,8 +141,6 @@ export const useFetchRequest = () => {
   //     setIsLoading(false);
   //   }
   // };
-  
-  
 
   return {
     alarmFetch,

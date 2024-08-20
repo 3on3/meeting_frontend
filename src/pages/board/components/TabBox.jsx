dc102c707c2383170e3a8ae7f387accdb41bcedf
munjieun <mun.mimi925@gmail.com>
2024-08-20 15:08:55 +0900

    :white_check_mark: 보드리스트 ui 구현

--- src/pages/board/components/TabBox.jsx
+++ src/pages/board/components/TabBox.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TabBox = ({styles,className}) => {
+const TabBox = ({styles,className, activeTab, setActiveTab}) => {
+
+    const onClickActive = (tab) => {
+      setActiveTab(tab); // 클릭된 탭의 상태를 설정
+    };
   return (
     <div className={className}>
-      <button className={`${styles.tabBtn} ${styles.isActive}`}>전체 글</button>
-      <button className={styles.tabBtn}>내가 쓴 글</button>
+  <button
+        className={`${styles.tabBtn} ${activeTab === 'all' ? styles.isActive : ''}`}
+        onClick={() => onClickActive('all')}
+      >
+        전체 글
+      </button>
+      <button
+        className={`${styles.tabBtn} ${activeTab === 'myPosts' ? styles.isActive : ''}`}
+        onClick={() => onClickActive('myPosts')}
+      >
+        내가 쓴 글
+      </button>
     </div>
   );
 };

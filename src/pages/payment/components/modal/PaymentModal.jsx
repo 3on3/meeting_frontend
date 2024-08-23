8f29846bae8b0d0920c596cebf663f3d59402842
yocong <rnfaos77@naver.com>
2024-08-23 16:05:26 +0900

    🐛 카카오페이 url 수정2

--- src/pages/payment/components/modal/PaymentModal.jsx
+++ src/pages/payment/components/modal/PaymentModal.jsx
@@ -11,7 +11,6 @@ const PaymentModal = ({ name, totalPrice, onCancel }) => {
   const handleConfirm = async () => {
     setIsLoading(true);
     setErrorMessage("");
-    console.log('PAYMENT_URL:', PAYMENT_URL);
 
     try {
       const response = await fetch(`${PAYMENT_URL}/ready`, {
@@ -23,38 +22,27 @@ const PaymentModal = ({ name, totalPrice, onCancel }) => {
         body: JSON.stringify({
           item_name: name,
           total_amount: totalPrice, 
-          partner_order_id: "1234", // 백엔드에서 사용하는 고정값으로 변경
+          partner_order_id: "unique_order_id",
           partner_user_id: getUserData()?.email, 
-          approval_url: `${PAYMENT_URL}/approval/`, 
-          cancel_url: `${API_BASE_URL}`,
-          fail_url: `${API_BASE_URL}`
+          approval_url: `${PAYMENT_URL}:80/approval`, 
+          cancel_url: `${API_BASE_URL}:80`,
+          fail_url: `${API_BASE_URL}:80`
         }),
       });
-      
-      const responseText = await response.text();
-      console.log('Raw server response:', responseText);
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}, message: ${responseText}`);
-      }
+      const data = await response.json();
 
-      let data;
-      try {
-        data = JSON.parse(responseText);
-      } catch (error) {
-        console.error('JSON parsing error:', error);
-        throw new Error('Invalid JSON response from server');
-      }
+      if (data && data.tid) {
+        localStorage.setItem("tid", data.tid);
+        localStorage.setItem("payUrl", data.next_redirect_mobile_url);
 
-      if (data && data.next_redirect_mobile_url) {
-        // tid는 서버에서 관리하므로 클라이언트에서 저장할 필요X
         window.location.href = data.next_redirect_mobile_url;
       } else {
         setErrorMessage("결제 준비에 실패했습니다.");
         console.error("결제 준비 응답 데이터가 올바르지 않습니다:", data);
       }
     } catch (error) {
-      setErrorMessage(`결제 준비 중 오류가 발생했습니다: ${error.message}`);
+      setErrorMessage("결제 준비 중 오류가 발생했습니다.");
       console.error("결제 준비 중 오류 발생:", error);
     } finally {
       setIsLoading(false);
